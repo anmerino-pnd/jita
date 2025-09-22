3e7a9147a1bed391235adec74dc11127f8d2b6f3
Angel Merino <acedeno00@gmail.com>
2025-09-22 13:00:20 -0700

    update

--- src/jita/settings/credentials.py
+++ src/jita/settings/credentials.py
@@ -5,3 +5,4 @@ from dotenv import load_dotenv
 qqp : str = os.getenv('QQP_DATOS')
 datos_abiertos: str = os.getenv('DATOS_ABIERTOS')
 gasolina_hmo: str = os.getenv('GASOLINA_HMO')
+casa_ley: str = os.getenv('CASA_LEY')
